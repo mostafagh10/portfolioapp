690492c232e103380a6f5d7b5042b04714e2617e
mostafa ghazaly <ghazalymostafa07@gmail.com>
2020-09-02 19:41:13 +0200

    Updates

---
+++ precache-manifest.6ec0ea36bed0c41f39530e725a9ba07e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb43457605e87c5b636cff37facf102",
+    "url": "/portfolioapp/index.html"
+  },
+  {
+    "revision": "bd6762efcac307d2f9fc",
+    "url": "/portfolioapp/static/css/2.814780ed.chunk.css"
+  },
+  {
+    "revision": "90e0334c1abb97c16963",
+    "url": "/portfolioapp/static/css/main.543b3993.chunk.css"
+  },
+  {
+    "revision": "bd6762efcac307d2f9fc",
+    "url": "/portfolioapp/static/js/2.423468ce.chunk.js"
+  },
+  {
+    "revision": "1e18bfd551dfb4232f48bc3e9f758b07",
+    "url": "/portfolioapp/static/js/2.423468ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90e0334c1abb97c16963",
+    "url": "/portfolioapp/static/js/main.aedd1775.chunk.js"
+  },
+  {
+    "revision": "77fe15d03fe8935992af",
+    "url": "/portfolioapp/static/js/runtime-main.fd7956b1.js"
+  }
+]);
